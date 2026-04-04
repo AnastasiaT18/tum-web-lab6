0e6e89004beb7b1ac59e87d8c01ebdc3bbc58ed7
Anastasia Tiganescu <145902828+AnastasiaT18@users.noreply.github.com>
2026-04-04 09:20:20 +0300

    Lab6 - Feature - App set up and layout (#1)
    * Configure layout
    
    * Change font family and brand color
    
    * Setup for deployment

---
+++ gainmap/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+function Navbar({darkMode, setDarkMode}) {
+    return (
+        <nav className="max-w-full mx-auto bg-stone-50 dark:bg-stone-900 px-6 py-8 ">
+            <div className="flex items-center justify-between">
+                <div>
+                    <h1 className="text-4xl font-medium text-stone-800 dark:text-stone-100">Gain<span className="text-brand">Map</span></h1>
+                    <p className="text-sm text-stone-800  dark:text-stone-100">Track your fitness journey</p>
+                </div>
+
+                <button onClick={() => setDarkMode(!darkMode)}
+                    className="text-sm px-3 py-1.5 rounded-lg border border-stone-200 dark:border-stone-600 text-stone-600 dark:text-stone-300 hover:bg-stone-100 dark:hover:bg-stone-700 transition-colors"
+                        >
+                    {darkMode ? "☀️ Light" : "🌙 Dark"}
+                </button>
+            </div>
+        </nav>
+    )
+
+
+}
+
+export default Navbar
