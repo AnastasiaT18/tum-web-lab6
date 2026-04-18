f213ec56b6f816171a9d92422d59267845d3dd55
Anastasia <anastasia.tiganescu@isa.utm.md>
2026-04-18 09:29:56 +0300

    Add show more and show less button

--- gainmap/src/components/RecentWorkouts.jsx
+++ gainmap/src/components/RecentWorkouts.jsx
@@ -1,9 +1,20 @@
 import { FiHeart, FiTrash2 } from "react-icons/fi";
 import { FaHeart } from "react-icons/fa";
 import toast from "react-hot-toast";
+import {useState} from "react";
+
+import { useRef } from "react";
+
 
 function RecentWorkouts({ workouts = [], onDelete, toggleLike }){
     const sortedWorkouts = [...workouts].sort((a,b) => new Date(b.date) - new Date(a.date)).slice(0,5);
+
+    const [visibleCount, setVisibleCount] = useState(2);
+
+    let visibleWorkouts = sortedWorkouts.slice(0, visibleCount);
+
+    const lastItemRef = useRef(null);
+
 
     const handleDelete = (id) => {
         if(window.confirm("Are you sure you want to delete this workout?")){
@@ -17,12 +28,20 @@ function RecentWorkouts({ workouts = [], onDelete, toggleLike }){
         toast(workout.liked ? "Workout unliked 👎" : "Workout liked ❤️");
     }
 
+    const handleShowMore = () => {
+        setVisibleCount(prev => prev + 1);
+
+        setTimeout(() => {
+            lastItemRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+        }, 100);
+    }
     return (
         <div >
             <h2 className="text-lg text-stone-800 dark:text-stone-200 mb-4">Recent Workouts</h2>
 
-            {sortedWorkouts.map(workout =>(
-                        <div key={workout.id} className=" p-4 rounded-lg hover:bg-stone-100 hover:border-stone-300 dark:hover:bg-stone-700 dark:hover:border-stone-600 transition-colors">
+            {visibleWorkouts.map((workout,index) =>(
+                        <div key={workout.id} ref={index === visibleWorkouts.length - 1 ? lastItemRef : null}
+                        className=" p-4 rounded-lg hover:bg-stone-100 hover:border-stone-300 dark:hover:bg-stone-700 dark:hover:border-stone-600 transition-colors">
                            <div className="flex">
                                 <p className="text-sm text-stone-500 dark:text-stone-400 mb-2">{new Date(workout.date).toLocaleDateString()}</p>
                                 <div className="ml-auto flex gap-2">
@@ -49,6 +68,27 @@ function RecentWorkouts({ workouts = [], onDelete, toggleLike }){
                         </div>
                     ))
                 }
+
+            
+                <div className="flex justify-center">
+                {visibleCount < workouts.length && (
+                    <button
+                        onClick={handleShowMore}
+                        className="mt-4 rounded-lg px-4 py-2 hover:bg-stone-200 dark:hover:bg-stone-600 transition-colors"
+                    >Show more
+                    </button>
+                )}
+
+                {visibleCount >= workouts.length && (
+                    <button
+                        onClick={() => setVisibleCount(2)}
+                        className="mt-4 rounded-lg px-4 py-2 hover:bg-stone-200 dark:hover:bg-stone-600 transition-colors"
+                    >Show less  
+                    </button>
+                )}
+
+                </div>
+            
             {workouts.length === 0 && (
                 <p className="text-stone-400 dark:text-stone-500">No workouts yet. 
                 </p>
