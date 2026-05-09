a2de0128dcef410b09f50f19101392dd3be2ce72
Anastasia <anastasia.tiganescu@isa.utm.md>
2026-05-09 16:14:53 +0300

    Make token expire in 1min

--- backend/routes/token.js
+++ backend/routes/token.js
@@ -33,7 +33,7 @@ router.post('/token', (req, res) => {
         return res.status(400).json({error: 'Role must be either ADMIN or VISITOR'});
     }
 
-    const token = jwt.sign({role}, process.env.JWT_SECRET, {expiresIn: '1h'});
+    const token = jwt.sign({role}, process.env.JWT_SECRET, {expiresIn: '1m'});
 
     res.json({token});
 
