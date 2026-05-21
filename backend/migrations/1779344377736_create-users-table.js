b22a060dcb6ab4ecb80575ab45074d2fbc294da2
Anastasia <anastasia.tiganescu@isa.utm.md>
2026-05-21 09:31:12 +0300

    Migrate db to add new users table and user_id column to workouts table

---
+++ backend/migrations/1779344377736_create-users-table.js
@@ -0,0 +1,44 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.horthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    //create users table
+    pgm.createTable('users', {
+        id: { type: 'serial', primaryKey: true },
+        email: {type: 'text', notNull: true, unique: true},
+        passwordHash: { type: 'text', notNull: true },
+        createdAt: { type: 'timestamp', default: pgm.func('NOW()') }
+    });
+
+    //add users_id column to workouts table
+    pgm.addColumn('workouts', {
+        user_id: {
+            type: 'integer',
+            references: '"users"',
+            referencesConstraintName: 'workouts_user_id_fkey',
+            onDelete: 'CASCADE',
+            notNull: false
+        }
+    });
+
+    pgm.createIndex('workouts', 'user_id');
+
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropIndex('workouts', 'user_id');
+    pgm.dropColumn('workouts', 'user_id');
+    pgm.dropTable('users');
+};
